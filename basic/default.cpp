5ef9741849766529caf3fcbfe14ff6be40d6a88c
owen0806 <147486323+owen0806@users.noreply.github.com>
2023-10-10 18:14:41 +0800

    Update default.cpp

--- basic/default.cpp
+++ basic/default.cpp
@@ -4,7 +4,13 @@
 #define PB push_back
 #define pii pair<int, int>
 using namespace std;
+void solve(){
+}
 signed main(){
   ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-  
+  int T = 1;
+  //cin >> T;
+  while(T--){
+    solve();
+  }
 }
