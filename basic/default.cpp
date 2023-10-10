044412b7f21d9f8c896e84a91cc6185a0391b366
owen0806 <147486323+owen0806@users.noreply.github.com>
2023-10-10 16:24:02 +0800

    Update default.cpp

--- basic/default.cpp
+++ basic/default.cpp
@@ -2,6 +2,7 @@
 #define int long long
 #define endl '\n'
 #define PB push_back
+#define pii pair<int, int>
 using namespace std;
 signed main(){
   ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
