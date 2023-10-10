6da4dc33c9f6bef52e7c542c04cca8a108bbf85d
owen0806 <147486323+owen0806@users.noreply.github.com>
2023-10-10 16:23:09 +0800

    Create default.cpp

---
+++ basic/default.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#define int long long
+#define endl '\n'
+#define PB push_back
+using namespace std;
+const int N = 2e5 + 5;
+signed main(){
+  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  
+}
