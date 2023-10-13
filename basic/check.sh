7453ccc552f76bc047f1e6c7c778c17f6a154f03
owen0806 <147486323+owen0806@users.noreply.github.com>
2023-10-13 11:11:21 +0800

    Create check.sh

---
+++ basic/check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+g++ ac.cpp -o ac
+g++ wa.cpp -o wa
+for((i=0;;i++))
+do
+    echo "$i"
+    python3 gen.py > input
+    ./ac < input > ac.out
+    ./wa < input > wa.out
+    diff ac.out wa.out || break
+done
